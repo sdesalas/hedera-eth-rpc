0f5c86f83e58dd783e9353e9508af80368542c66
Steven de Salas <steven@desalasworks.com>
2022-05-06 20:31:56 +0200

    Review deployment

--- .deploy/check.sh
+++ .deploy/check.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 # Helps redeploy services when there are git remote changes
+#
+# @example: sudo ./check.sh origin/master --force
 
 FETCH=$(git fetch)
 UPSTREAM=${1:-'origin/master'}
@@ -8,12 +10,12 @@ LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 
-if [ $LOCAL = $REMOTE ]; then
-    echo "$(date +%FT%T) Up-to-date"
-elif [ $LOCAL = $BASE ]; then
+if [ $LOCAL = $BASE ] || [ "$2" = "--force"]; then
     echo "$(date +%FT%T) Need to pull"
     git pull
     docker-compose -f ${BASH_SOURCE%/*}/api/docker-compose.yml up --build -d
+elif [ $LOCAL = $REMOTE ]; then
+    echo "$(date +%FT%T) Up-to-date"
 elif [ $REMOTE = $BASE ]; then
     echo "$(date +%FT%T) Need to push"
 else
