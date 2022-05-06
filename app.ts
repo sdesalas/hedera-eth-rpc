1340bf22b953019eb77ecd8176d4809ef0257992
Steven de Salas <steven@desalasworks.com>
2022-05-06 16:42:20 +0200

    Reduce attack surface area

--- app.ts
+++ app.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json({limit: '10mb'}));
 
 app.get('/healthcheck', (req, res) => res.status(200).send('OK'));
-
+app.get('/', (req, res) => res.end());
 app.post('/', jsonRPC);
 
 // Finish off
