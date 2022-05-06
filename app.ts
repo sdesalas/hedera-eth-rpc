1d0f3c657bc66a82fb780a699bbc7fcb7ad7a4f6
Steven de Salas <steven@desalasworks.com>
2022-05-06 21:01:35 +0200

    Test CD

--- app.ts
+++ app.ts
@@ -8,7 +8,7 @@ app.disable('x-powered-by');
 app.use(express.json({limit: '10mb'}));
 app.use((req, res, next) => { res.removeHeader('x-content-type-options'); next(); });
 
-app.get('/healthcheck', (req, res) => res.status(200).send('OK'));
+app.get('/healthcheck', (req, res) => res.status(200).send('test'));
 app.get('/', (req, res) => res.connection.destroy());
 app.post('/', jsonRPC);
 
