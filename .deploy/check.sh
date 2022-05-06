b934ba4f6754072895d4e82497f1cdde39e681df
Steven de Salas <steven@desalasworks.com>
2022-05-06 20:55:58 +0200

    Deploy

--- .deploy/check.sh
+++ .deploy/check.sh
@@ -10,15 +10,24 @@ LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 
-if [ $LOCAL = $BASE ] || [ "$2" = "--force"]; then
-    echo "$(date +%FT%T) Need to pull"
+function rebuild() {
     git pull
     docker-compose -f ${BASH_SOURCE%/*}/api/docker-compose.yml up --build -d
+}
+
+if [ "$2" = "--force" ]; then
+    echo "$(date +%FT%T) --force rebuild"
+    rebuild
 elif [ $LOCAL = $REMOTE ]; then
     echo "$(date +%FT%T) Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "$(date +%FT%T) Need to pull"
+    rebuild
 elif [ $REMOTE = $BASE ]; then
     echo "$(date +%FT%T) Need to push"
 else
     echo "$(date +%FT%T) Diverged"
 fi
+
+
 
