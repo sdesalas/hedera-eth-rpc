f0cd40c788c3f46d5ecee45b80180beb1b9ca3d4
Steven de Salas <steven@desalasworks.com>
2022-05-07 12:43:27 +0200

    Review crawler logic

--- src/lib/crawler.ts
+++ src/lib/crawler.ts
@@ -3,56 +3,68 @@ import MongoDb from './mongodb';
 import { toEthAddress } from './utils';
 import axios from 'axios';
 
+const HEDERA_MIRROR_API_ROOT = {
+  mainnet: 'https://mainnet-public.mirrornode.hedera.com',
+  testnet: 'https://testnet.mirrornode.hedera.com'
+ };
+
 export default class Crawler {
-  static HEDERA_MIRROR_API_ROOT = (config.HEDERA_ETHRPC_NETWORK === 'mainnet') ? 
-          'https://mainnet-public.mirrornode.hedera.com' : 
-          'https://testnet.mirrornode.hedera.com';
-
-  static nextUrl;
-
-  static startUrl = `${Crawler.HEDERA_MIRROR_API_ROOT}/api/v1/accounts?balance=false&limit=100&order=desc`;
-
-  static start() {
-    console.log(Date.now(), 'Crawler.start()')
-    setInterval(Crawler.heartbeat, config.HEDERA_ETHRPC_HEARTBEAT_MS);
+  constructor(network:string) {
+    console.log('%s new Crawler(%s)', Date.now(), network);
+    if (!Object.keys(HEDERA_MIRROR_API_ROOT).includes(network)) {
+      throw new Error('Network unavailable: ' + network);
+    } else {
+      this.network = network;
+    }
   }
 
-  static async heartbeat() {
-    console.log(Date.now(), 'Crawler.heartbeat()');
-    const url = Crawler.nextUrl || Crawler.startUrl;
-    console.log(`GET ${url}`);
-    const response = await axios.get(url);
-    console.log(`${response.status} ${response.statusText}`);
-    console.log(`${JSON.stringify(response.data)}`);
-    const accounts = response.data?.accounts;
-    if (Array.isArray(accounts)) {
-      const items = [];
-      for(const a of accounts) {
-        if (a?.key?.key) {
-          const _id = toEthAddress('0x' + a.key.key);
-          const key = a.key.key;
-          const type = a.key._type;
-          const item = {_id, key, type, accounts: {$concatArrays: [{$ifNull: ["$accounts", []]}, {$cond: {if: {$in: [a.account, {$ifNull: ["$accounts", []]}]}, then: [], else: [a.account]}}]}};
-          // const item = {_id, key, type, accounts: {$addToSet: a.account}};
-          console.log('item', item);
-          items.push(item);
+  network = 'mainnet';
+  nextUrl;
+  startUrl = `/api/v1/accounts?balance=false&limit=100&order=desc&account.id=lt:0.0.514186`;
+
+  start() {
+    console.log('%s Crawler.start()', Date.now());
+    setInterval(() => this.heartbeat(), config.HEDERA_ETHRPC_HEARTBEAT_MS);
+  }
+
+  async heartbeat() {
+    console.log('%s Crawler.heartbeat()', Date.now());
+    const apiRoot = HEDERA_MIRROR_API_ROOT[this.network];
+    const url = this.nextUrl || this.startUrl;
+    try {
+      console.log(`GET ${apiRoot}${url}`);
+      const response = await axios.get(`${apiRoot}${url}`);
+      console.log(`${response.status} ${response.statusText}`);
+      console.log(`${JSON.stringify(response.data)}`);
+      const accounts = response.data?.accounts;
+      if (Array.isArray(accounts)) {
+        const items = [];
+        for(const a of accounts) {
+          if (a?.key?.key) {
+            const _id = toEthAddress('0x' + a.key.key);
+            const key = a.key.key;
+            const type = a.key._type;
+            const item = {_id, key, type, accounts: {$concatArrays: [{$ifNull: ["$accounts", []]}, {$cond: {if: {$in: [a.account, {$ifNull: ["$accounts", []]}]}, then: [], else: [a.account]}}]}};
+            // const item = {_id, key, type, accounts: {$addToSet: a.account}};
+            console.log('item', item);
+            items.push(item);
+          }
         }
-      }
-      try {
+        // Bulk insert
         const connection = await MongoDb.connect();
-        const c = await connection.db(config.MONGODB_DB).collection(config.HEDERA_ETHRPC_NETWORK);
+        const c = await connection.db(config.MONGODB_DB).collection(this.network);
         const ops = items.map(item => ({ updateOne: { filter: { _id: item._id }, update: [{ $set: item }], upsert: true}}));
         const docs = await c.bulkWrite(ops, {ordered: false});
         console.log('bulkWrite() upserted #', docs.nUpserted);
         console.log('bulkWrite() matched #', docs.nMatched);
         console.log('bulkWrite() modified #', docs.nModified);
-      } catch (e) {
-        console.error(e);
-        throw e;
       }
+      // Pick next url or start over (only if everything went ok)
+      const next = response.data?.links?.next;
+      this.nextUrl = (next) ? next: this.startUrl;
+    } catch (e) {
+      console.error(e);
+      throw e;
     }
-    // Pick next url or start over
-    const next = response.data?.links?.next;
-    Crawler.nextUrl = (next) ? `${Crawler.HEDERA_MIRROR_API_ROOT}${next}`: Crawler.startUrl;
   }
 }
